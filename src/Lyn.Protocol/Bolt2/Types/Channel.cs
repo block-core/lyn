de915b51a58400c3e311d5e881de04e0394b82a2
dangershony <dan.gershony@gmail.com>
2021-06-16 23:09:56 +0100

    Create the Channel Established interface

---
+++ src/Lyn.Protocol/Bolt2/Types/Channel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lyn.Protocol.Bolt3.Types;
+using Lyn.Types.Bitcoin;
+using Lyn.Types.Bolt;
+using Lyn.Types.Fundamental;
+
+namespace Lyn.Protocol.Bolt2.Types
+{
+    public class Channel
+    {
+        public ChannelId? ChannelId { get; set; }
+
+        public OutPoint? FundingOutpoint { get; set; }
+
+        /* Keys used to spend funding tx. */
+        public PublicKey? FundingPubkey { get; set; }
+
+        /* satoshis in from commitment tx */
+        public Satoshis Funding { get; set; }
+
+        /* confirmations needed for locking funding */
+        public uint MinimumDepth { get; set; }
+
+        /* Who is paying fees. */
+        public ChannelSide Opener { get; set; }
+
+        /* Limits and settings on this channel. */
+        public ChannelConfig Config { get; set; }
+
+        /* Basepoints for deriving keys. */
+
+        public PublicKey Basepoints { get; set; }
+
+        /* Mask for obscuring the encoding of the commitment number. */
+        public ulong CommitmentNumberObscurer { get; set; }
+
+        /* All live HTLCs for this channel */
+        public Dictionary<OutPoint, Htlc> Htlcs { get; set; }
+
+        /* Fee changes, some which may be in transit */
+        public Satoshis FeeStates { get; set; }
+
+        /* What it looks like to each side. */
+        public ChannelView ChannelView { get; set; }
+
+        /* Is this using option_static_remotekey? */
+        public bool OptionStaticRemotekey { get; set; }
+
+        /* Is this using option_anchor_outputs? */
+        public bool OptionAnchorOutputs { get; set; }
+    }
+}
---
+++ src/Lyn.Protocol/Bolt2/Types/ChannelView.cs
@@ -0,0 +1,12 @@
+﻿using Lyn.Types.Fundamental;
+
+namespace Lyn.Protocol.Bolt2.Types
+{
+    /// <summary>
+    /// How much is owed to each side (includes pending changes)
+    /// </summary>
+    public class ChannelView
+    {
+        private MiliSatoshis Amountmsat { get; set; }
+    };
+}
