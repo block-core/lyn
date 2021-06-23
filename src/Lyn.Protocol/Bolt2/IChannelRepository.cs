24c6b28fafdbe027b33222f64b432179141aa8a1
dangershony <dan.gershony@gmail.com>
2021-06-23 17:30:02 +0100

    Create the channel state class

---
+++ src/Lyn.Protocol/Bolt2/IChannelRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lyn.Protocol.Bolt2.Entities;
+using Lyn.Types.Bolt;
+
+namespace Lyn.Protocol.Bolt2
+{
+    public interface IChannelRepository
+    {
+        void AddOrUpdate(ChannelState channelState);
+
+        ChannelState Get(ChannelId channelId);
+    }
+}
