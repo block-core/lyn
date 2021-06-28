cebc95a24925debacbffc7310bebd41deb19ccd7
dangershony <dan.gershony@gmail.com>
2021-06-28 17:50:03 +0100

    Remove comments that are from the bolt specification

--- src/Lyn.Protocol/Bolt2/ChannelEstablishment/OpenChannelMessageService.cs
+++ src/Lyn.Protocol/Bolt2/ChannelEstablishment/OpenChannelMessageService.cs
@@ -67,10 +67,6 @@ namespace Lyn.Protocol.Bolt2.ChannelEstablishment
 
             if (currentState != null)
             {
-                // Bolt 2 - if the connection has been re-established after receiving a previous open_channel, BUT before receiving a funding_created message:
-                // accept a new open_channel message.
-                //    discard the previous open_channel message.
-
                 // todo: dan write this logic
                 return;
             }
@@ -79,8 +75,6 @@ namespace Lyn.Protocol.Bolt2.ChannelEstablishment
 
             if (chainParameters == null)
             {
-                // Bolt 2 - the chain_hash value is set to a hash of a chain that is unknown to the receiver.
-
                 // todo: fail the channel.
                 return;
             }
@@ -89,8 +83,6 @@ namespace Lyn.Protocol.Bolt2.ChannelEstablishment
 
             if (channelConfig == null)
             {
-                // This is an internal issue but we still need to fail the channel
-
                 // todo: fail the channel.
                 return;
             }
@@ -103,7 +95,6 @@ namespace Lyn.Protocol.Bolt2.ChannelEstablishment
                 return;
             }
 
-            // Create the channel durable state
             ChannelState channelState = new()
             {
                 FundingAmount = openChannel.FundingSatoshis,
@@ -136,15 +127,12 @@ namespace Lyn.Protocol.Bolt2.ChannelEstablishment
 
             acceptChannel.FundingPubkey = _lightningKeyDerivation.PublicKeyFromPrivateKey(secrets.FundingPrivkey);
 
-            // Bolt 2 - MUST set funding_pubkey, revocation_basepoint, htlc_basepoint, payment_basepoint,
-            // and delayed_payment_basepoint to valid secp256k1 pubkeys in compressed format
             Basepoints basepoints = _lightningKeyDerivation.DeriveBasepoints(secrets);
             acceptChannel.RevocationBasepoint = basepoints.Revocation;
             acceptChannel.HtlcBasepoint = basepoints.Htlc;
             acceptChannel.PaymentBasepoint = basepoints.Payment;
             acceptChannel.DelayedPaymentBasepoint = basepoints.DelayedPayment;
 
-            // Bolt 2 - MUST set first_per_commitment_point to the per-commitment point to be used for the initial commitment transaction, derived as specified in BOLT #3.
             acceptChannel.FirstPerCommitmentPoint = _lightningKeyDerivation.PerCommitmentPoint(secrets.Shaseed, 0);
 
             channelState.LocalPublicKey = acceptChannel.FundingPubkey;
@@ -159,36 +147,12 @@ namespace Lyn.Protocol.Bolt2.ChannelEstablishment
 
         private string CheckMessage(OpenChannel openChannel, ChainParameters chainParameters, ChannelConfig channelConfig)
         {
-            // Bolt 2 -
-            //  The receiving node MAY fail the channel if:
-            //    announce_channel is false(0), yet it wishes to publicly announce the channel.
-            //    funding_satoshis is too small.
-            //    it considers htlc_minimum_msat too large.
-            //    it considers max_htlc_value_in_flight_msat too small.
-            //    it considers channel_reserve_satoshis too large.
-            //    it considers max_accepted_htlcs too small.
-            //    it considers dust_limit_satoshis too small and plans to rely on the sending node publishing its commitment
-            //       transaction in the event of a data loss(see message-retransmission).
-
             if (openChannel.FundingSatoshis < chainParameters.MinFundingAmount) return "funding_satoshis is too small";
             if (openChannel.HtlcMinimumMsat > channelConfig.HtlcMinimum) return "htlc_minimum_msat too large";
             if (openChannel.MaxHtlcValueInFlightMsat < channelConfig.MaxHtlcValueInFlight) return "max_htlc_value_in_flight_msat too small";
             if (openChannel.ChannelReserveSatoshis > channelConfig.ChannelReserve) return "channel_reserve_satoshis too large";
             if (openChannel.MaxAcceptedHtlcs < channelConfig.MaxAcceptedHtlcs) return "max_accepted_htlcs too small";
             if (openChannel.DustLimitSatoshis < channelConfig.DustLimit) return "dust_limit_satoshis too small";
-
-            // Bolt 2 -
-            //  The receiving node MUST fail the channel if:
-            //    the chain_hash value is set to a hash of a chain that is unknown to the receiver.
-            //    push_msat is greater than funding_satoshis * 1000.
-            //    to_self_delay is unreasonably large.
-            //    max_accepted_htlcs is greater than 483.
-            //    it considers feerate_per_kw too small for timely processing or unreasonably large.
-            //    funding_pubkey, revocation_basepoint, htlc_basepoint, payment_basepoint, or delayed_payment_basepoint are not valid secp256k1 pubkeys in compressed format.
-            //    dust_limit_satoshis is greater than channel_reserve_satoshis.
-            //    the funder's amount for the initial commitment transaction is not sufficient for full fee payment.
-            //    both to_local and to_remote amounts for the initial commitment transaction are less than or equal to channel_reserve_satoshis(see BOLT 3).
-            //    funding_satoshis is greater than or equal to 2 ^ 24 and the receiver does not support option_support_large_channel.
 
             MiliSatoshis fundingMiliSatoshis = openChannel.FundingSatoshis;
             if (openChannel.PushMsat > fundingMiliSatoshis) return "push_msat is greater than funding_satoshis * 1000";
@@ -213,7 +177,7 @@ namespace Lyn.Protocol.Bolt2.ChannelEstablishment
             if ((openChannel.FundingSatoshis > chainParameters.LargeChannelAmount)
                 && _boltFeatures.SupportedFeatures != Features.OptionSupportLargeChannel) return "funding_satoshis too big for option_support_large_channel";
 
-            return String.Empty;
+            return string.Empty;
         }
     }
 }
--- src/Lyn.Protocol/Bolt2/ChannelEstablishment/StartOpenChannelService.cs
+++ src/Lyn.Protocol/Bolt2/ChannelEstablishment/StartOpenChannelService.cs
@@ -77,17 +77,10 @@ namespace Lyn.Protocol.Bolt2.ChannelEstablishment
 
             OpenChannel openChannel = new();
 
-            // Bolt 2 - MUST ensure the chain_hash value identifies the chain it wishes to open the channel within.
             openChannel.ChainHash = chainParameters.GenesisBlockhash;
 
-            // Bolt 2 - MUST ensure temporary_channel_id is unique from any other channel ID with the same peer
             openChannel.TemporaryChannelId = new ChannelId(_randomNumberGenerator.GetBytes(32));
 
-            // Bolt 2 -
-            // if both nodes advertised option_support_large_channel:
-            // MAY set funding_satoshis greater than or equal to 2 ^ 24 satoshi.
-            //    otherwise:
-            // MUST set funding_satoshis to less than 2 ^ 24 satoshi.
             bool localSupportLargChannels = _boltFeatures.SupportedFeatures == Features.OptionSupportLargeChannel;
             bool remoteSupportLargChannels = _parseFeatureFlags.ParseFeatures(peer.Featurs) == Features.OptionSupportLargeChannel;
             if (localSupportLargChannels == false || remoteSupportLargChannels == false)
@@ -98,7 +91,6 @@ namespace Lyn.Protocol.Bolt2.ChannelEstablishment
 
             openChannel.FundingSatoshis = startOpenChannelIn.FundingAmount;
 
-            // Bolt 2 - MUST set push_msat to equal or less than 1000 * funding_satoshis.
             MiliSatoshis fundingMiliSatoshis = startOpenChannelIn.FundingAmount;
             if (startOpenChannelIn.PushOnOpen > fundingMiliSatoshis)
                 throw new ApplicationException($"Not enough capacity to pay peer {startOpenChannelIn.PushOnOpen}msat on opening of the channel with capacity {fundingMiliSatoshis}msat");
@@ -110,44 +102,24 @@ namespace Lyn.Protocol.Bolt2.ChannelEstablishment
 
             openChannel.FundingPubkey = _lightningKeyDerivation.PublicKeyFromPrivateKey(secrets.FundingPrivkey);
 
-            // Bolt 2 - MUST set funding_pubkey, revocation_basepoint, htlc_basepoint, payment_basepoint,
-            // and delayed_payment_basepoint to valid secp256k1 pubkeys in compressed format
             Basepoints basepoints = _lightningKeyDerivation.DeriveBasepoints(secrets);
             openChannel.RevocationBasepoint = basepoints.Revocation;
             openChannel.HtlcBasepoint = basepoints.Htlc;
             openChannel.PaymentBasepoint = basepoints.Payment;
             openChannel.DelayedPaymentBasepoint = basepoints.DelayedPayment;
 
-            // Bolt 2 - MUST set first_per_commitment_point to the per-commitment point to be used for the initial commitment transaction, derived as specified in BOLT #3.
             openChannel.FirstPerCommitmentPoint = _lightningKeyDerivation.PerCommitmentPoint(secrets.Shaseed, 0);
 
-            // Bolt 2 - MUST set channel_reserve_satoshis greater than or equal to dust_limit_satoshis.
             if (channelConfig.ChannelReserve < channelConfig.DustLimit)
                 throw new ApplicationException($"ChannelReserve = {channelConfig.ChannelReserve}sat must be greater then DustLimit = {channelConfig.DustLimit}sat");
 
             openChannel.ChannelReserveSatoshis = channelConfig.ChannelReserve;
 
-            // Bolt 2 - MUST set undefined bits in channel_flags to 0.
             openChannel.ChannelFlags = startOpenChannelIn.PrivateChannel ? (byte)0 : (byte)ChannelFlags.ChannelFlags.AnnounceChannel;
-
-            // Bolt 2 - set to_self_delay sufficient to ensure the sender can irreversibly spend a commitment transaction output, in case of misbehavior by the receiver.
             openChannel.ToSelfDelay = channelConfig.ToSelfDelay;
-
-            // Bolt 2 - set feerate_per_kw to at least the rate it estimates would cause the transaction to be immediately included in a block.
             openChannel.FeeratePerKw = startOpenChannelIn.FeeRate;
-
-            // Bolt 2 - set dust_limit_satoshis to a sufficient value to allow commitment transactions to propagate through the Bitcoin network.
             openChannel.DustLimitSatoshis = channelConfig.DustLimit;
-
-            // Bolt 2 - set htlc_minimum_msat to the minimum value HTLC it's willing to accept from this peer.
             openChannel.HtlcMinimumMsat = channelConfig.HtlcMinimum;
-
-            // Bolt 2 - if both nodes advertised the option_upfront_shutdown_script feature:
-            // MUST include upfront_shutdown_script with either a valid shutdown_scriptpubkey as required by shutdown scriptpubkey, or a zero - length shutdown_scriptpubkey(ie. 0x0000).
-            //    otherwise:
-            // MAY include upfront_shutdown_script.
-            //    if it includes open_channel_tlvs:
-            // MUST include upfront_shutdown_script.
 
             // todo: dan create the shutdown_scriptpubkey once tlv is done.
 
@@ -158,7 +130,6 @@ namespace Lyn.Protocol.Bolt2.ChannelEstablishment
                 }
             };
 
-            // Create the channel durable state
             ChannelState channelState = new()
             {
                 FundingAmount = openChannel.FundingSatoshis,
