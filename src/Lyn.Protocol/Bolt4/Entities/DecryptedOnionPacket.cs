d40867475d4275d5cc4ccce84c52248762f6328d
Pegasus J. Crawford II <99172754+pegasusjcrawford@users.noreply.github.com>
2022-02-28 01:23:03 -0800

    some base entities for onion routing. eclair inspired

---
+++ src/Lyn.Protocol/Bolt4/Entities/DecryptedOnionPacket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyn.Protocol.Bolt4.Entities
+{
+    public class DecryptedOnionPacket
+    {
+
+        public byte[] Payload { get; set; }
+
+        public OnionRoutingPacket NextPacket { get; set; }
+
+        public byte[] SharedSecret { get; set; }
+
+        public bool IsLastPacket => (NextPacket?.Hmac == null || NextPacket?.Hmac.Length == 0);
+
+    }
+}
---
+++ src/Lyn.Protocol/Bolt4/Entities/OnionRoutingPacket.cs
@@ -0,0 +1,22 @@
+﻿using Lyn.Types.Fundamental;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyn.Protocol.Bolt4.Entities
+{
+    public class OnionRoutingPacket
+    {
+
+        public byte Version { get; set; }
+
+        public PublicKey EphemeralKey { get; set; }
+
+        public byte[] PayloadData { get; set; } 
+
+        public byte[] Hmac { get; set; }
+
+    }
+}
