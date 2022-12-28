d57730e0973dbb1524d828f5dc8a4b9fea1196ce
Pegasus J. Crawford II <pegasus.j.crawford@protonmail.com>
2022-12-28 13:51:46 -0800

    get testr to the point where i can concatenate routes and verify the payloads & keys look appropriate

--- src/Lyn.Protocol.Tests/Bolt4/RouteBlindingTests.cs
+++ src/Lyn.Protocol.Tests/Bolt4/RouteBlindingTests.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using Lyn.Types.Fundamental;
 using Lyn.Types.Onion;
 using Lyn.Protocol.Bolt3;
+using System.Diagnostics;
 
 namespace Lyn.Protocol.Tests.Bolt4
 {
@@ -19,34 +20,67 @@ namespace Lyn.Protocol.Tests.Bolt4
         [Fact]
         public void RouteBlinding_CanCreatedBlindedRoute_ReferenceTestVector()
         {
+            // note: this is really ugly rn, will clean up once e2e works
+
+            var lightningKeyDerivation = new LightningKeyDerivation();
+
             var alice = new PrivateKey(Convert.FromHexString("4141414141414141414141414141414141414141414141414141414141414141"));
+            var alicePubKey = lightningKeyDerivation.PublicKeyFromPrivateKey(alice);
             var bob = new PrivateKey(Convert.FromHexString("4242424242424242424242424242424242424242424242424242424242424242"));
+            var bobPubKey = lightningKeyDerivation.PublicKeyFromPrivateKey(bob);
             var bobPayload = Convert.FromHexString("01200000000000000000000000000000000000000000000000000000000000000000020800000000000000010a0800320000000027100c05000b7246320e00");
             var carol = new PrivateKey(Convert.FromHexString("4343434343434343434343434343434343434343434343434343434343434343"));
+            var carolPubKey = lightningKeyDerivation.PublicKeyFromPrivateKey(carol);
             var carolPayload = Convert.FromHexString("020800000000000000020821031b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f0a07004b00000096640c05000b7214320e00");
             var dave = new PrivateKey(Convert.FromHexString("4444444444444444444444444444444444444444444444444444444444444444"));
+            var davePubKey = lightningKeyDerivation.PublicKeyFromPrivateKey(dave);
             var davePayload = Convert.FromHexString("012200000000000000000000000000000000000000000000000000000000000000000000020800000000000000030a060019000000640c05000b71c9320e00");
             var eve = new PrivateKey(Convert.FromHexString("4545454545454545454545454545454545454545454545454545454545454545"));
+            var evePubKey = lightningKeyDerivation.PublicKeyFromPrivateKey(eve);
             var evePayload = Convert.FromHexString("011c000000000000000000000000000000000000000000000000000000000616c9cf92f45ade68345bc20ae672e2012f4af487ed44150c05000b71b0320e00");
 
             var curveActions = new EllipticCurveActions();
-            var lightningKeyDerivation = new LightningKeyDerivation();
             var sphinx = new Sphinx(curveActions);
 
             var routeBlinding = new RouteBlinding(sphinx, lightningKeyDerivation, curveActions);
 
             // Eve creates a blinded route to herself through Dave
-            var eveSelfSessionKey = new PrivateKey(Convert.FromHexString("0101010101010101010101010101010101010101010101010101010101010101"));
-            var daveEveHops = new List<(PublicKey PublicKey, byte[] Payload)>() { 
-                                (PublicKey: lightningKeyDerivation.PublicKeyFromPrivateKey(dave), Payload: davePayload), 
-                                (PublicKey: lightningKeyDerivation.PublicKeyFromPrivateKey(eve), Payload: evePayload) 
+            var eveSessionKey = new PrivateKey(Convert.FromHexString("0101010101010101010101010101010101010101010101010101010101010101"));
+            var daveEveHops = new List<(PublicKey PublicKey, byte[] Payload)>() {
+                                (PublicKey: davePubKey, Payload: davePayload),
+                                (PublicKey: evePubKey, Payload: evePayload)
                             };
-            var (BlindedRoute, LastBlinding) = routeBlinding.Create(eveSelfSessionKey, daveEveHops);
-            // this passes, hooray!
-            Assert.Equal(new PublicKey(Convert.FromHexString("031b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f")), BlindedRoute.BlindingKey);
-            // this fails, boo! 
-            // the failure stems from the result of ```e*PrivKey(sha256(blindingPubKeyBytes.Concat(sharedSecret)))``` being incorrect
-            Assert.Equal(new PublicKey(Convert.FromHexString("03e09038ee76e50f444b19abf0a555e8697e035f62937168b80adf0931b31ce52a")), LastBlinding);
+            var (blindedRouteEnd, lastBlinding) = routeBlinding.Create(eveSessionKey, daveEveHops);
+            Assert.Equal(new PublicKey(Convert.FromHexString("031b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f")), blindedRouteEnd.BlindingKey);
+            Assert.Equal(new PublicKey(Convert.FromHexString("03e09038ee76e50f444b19abf0a555e8697e035f62937168b80adf0931b31ce52a")), lastBlinding);
+
+            // Bob also wants to use route blinding:
+            var bobSessionKey = new PrivateKey(Convert.FromHexString("0202020202020202020202020202020202020202020202020202020202020202"));
+            var bobCarolHops = new List<(PublicKey PublicKey, byte[] Payload)>() {
+                                (PublicKey: lightningKeyDerivation.PublicKeyFromPrivateKey(bob), Payload: bobPayload),
+                                (PublicKey: lightningKeyDerivation.PublicKeyFromPrivateKey(carol), Payload: carolPayload)
+                            };
+            var blindedRouteStart = routeBlinding.Create(bobSessionKey, bobCarolHops).Route;
+            Assert.Equal(new PublicKey(Convert.FromHexString("024d4b6cd1361032ca9bd2aeb9d900aa4d45d9ead80ac9423374c451a7254d0766")), blindedRouteStart.BlindingKey);
+
+            // We now have a blinded route Bob -> Carol -> Dave -> Eve
+            var blindedRoute = new BlindedRoute(bobPubKey,
+                                                blindedRouteStart.BlindingKey,
+                                                blindedRouteStart.BlindedNodes.Concat(blindedRouteEnd.BlindedNodes).ToArray());
+
+            Assert.Equal(new List<PublicKey>() {
+                new PublicKey(Convert.FromHexString("03da173ad2aee2f701f17e59fbd16cb708906d69838a5f088e8123fb36e89a2c25")),
+                new PublicKey(Convert.FromHexString("02e466727716f044290abf91a14a6d90e87487da160c2a3cbd0d465d7a78eb83a7")),
+                new PublicKey(Convert.FromHexString("036861b366f284f0a11738ffbf7eda46241a8977592878fe3175ae1d1e4754eccf")),
+                new PublicKey(Convert.FromHexString("021982a48086cb8984427d3727fe35a03d396b234f0701f5249daa12e8105c8dae")),
+            }, blindedRoute.BlindedNodeIds);
+
+            Assert.Equal(new List<byte[]>() {
+                Convert.FromHexString("cd7b00ff9c09ed28102b210ac73aa12d63e90852cebc496c49f57c499a2888b49f2e72b19446f7e60a818aa2938d8c625415b992b8928a7321edb8f7cea40de362bed082ad51acc6156dca5532fb68"),
+                Convert.FromHexString("cc0f16524fd7f8bb0f4e8d40ad71709ef140174c76faa574cac401bb8992fef76c4d004aa485dd599ed1cf2715f570f656a5aaecaf1ee8dc9d0fa1d424759be1932a8f29fac08bc2d2a1ed7159f28b"),
+                Convert.FromHexString("0fa1a72cff3b64a3d6e1e4903cf8c8b0a17144aeb249dcb86561adee1f679ee8db3e561d9e49895fd4bcebf6f58d6f61a6d41a9bf5aa4b0453437856632e8255c351873143ddf2bb2b0832b091e1b4"),
+                Convert.FromHexString("da1c7e5f7881219884beae6ae68971de73bab4c3055d9865b1afb60722a63c688768042ade22f2c22f5724767d171fd221d3e579e43b354cc72e3ef146ada91a892d95fc48662f5b158add0af457da")
+            }, blindedRoute.EncryptedPayloads.ToList());
         }
 
     }
--- src/Lyn.Protocol/Bolt4/RouteBlinding.cs
+++ src/Lyn.Protocol/Bolt4/RouteBlinding.cs
@@ -11,47 +11,50 @@ using NaCl.Core;
 namespace Lyn.Protocol.Bolt4
 {
 
+    public record IntroductionNode(PublicKey PublicKey, PublicKey BlindedPublicKey, PublicKey BlindingEphemeralKey, byte[] EncryptedPayload);
+
+    public record BlindedNode(PublicKey BlindedPublicKey, byte[] EncryptedPayload);
+
+    public record BlindedRoute
+    {
+
+        // todo: Are these two properties needed? The other properties contain the same information?
+        public PublicKey IntroductionNodeId { get; init; }
+        public PublicKey BlindingKey { get; init; }
+
+        public IntroductionNode IntroductionNode { get; init; }
+        public IEnumerable<BlindedNode> SubsequentNodes { get; private set; }
+        public IEnumerable<PublicKey> BlindedNodeIds { get; private set; }
+        public IEnumerable<byte[]> EncryptedPayloads { get; private set; }
+
+
+        public BlindedNode[] BlindedNodes { get; init; }
+
+        public BlindedRoute(PublicKey introductionNodeId, PublicKey blindingKey, BlindedNode[] blindedNodes)
+        {
+            if (blindedNodes.Length == 0)
+            {
+                throw new ArgumentException(nameof(blindedNodes), "blinded route must not be empty");
+            }
+
+            IntroductionNodeId = introductionNodeId;
+            BlindingKey = blindingKey;
+            BlindedNodes = blindedNodes;
+
+            IntroductionNode = new IntroductionNode(IntroductionNodeId, BlindedNodes.First().BlindedPublicKey, BlindingKey, BlindedNodes.First().EncryptedPayload);
+            SubsequentNodes = BlindedNodes.Skip(1);
+            BlindedNodeIds = BlindedNodes.Select(x => x.BlindedPublicKey);
+            EncryptedPayloads = BlindedNodes.Select(x => x.EncryptedPayload);
+        }
+    }
+
+    public record BlindedRouteDetails(BlindedRoute Route, PublicKey LastBlinding);
+
     public class RouteBlinding
     {
 
-        public record IntroductionNode(PublicKey PublicKey, PublicKey BlindedPublicKey, PublicKey BlindingEphemeralKey, byte[] EncryptedPayload);
-
-        public record BlindedNode(PublicKey BlindedPublicKey, byte[] EncryptedPayload);
-
-        public record BlindedRoute
-        {
-
-            // todo: Are these two properties needed? The other properties contain the same information?
-            public PublicKey IntroductionNodeId { get; init; }
-            public PublicKey BlindingKey { get; init; }
-            
-            public IntroductionNode IntroductionNode { get; init; }
-            public IEnumerable<BlindedNode> SubsequentNodes { get; private set; }
-            public IEnumerable<PublicKey> BlindedNodeIds { get; private set; }
-            public IEnumerable<byte[]> EncryptedPayloads { get; private set; }
 
 
-            public BlindedNode[] BlindedNodes { get; init; }
-
-            public BlindedRoute(PublicKey introductionNodeId, PublicKey blindingKey, BlindedNode[] blindedNodes)
-            {
-                if (blindedNodes.Length == 0)
-                {
-                    throw new ArgumentException(nameof(blindedNodes), "blinded route must not be empty");
-                }
-
-                IntroductionNodeId = introductionNodeId;
-                BlindingKey = blindingKey;
-                BlindedNodes = blindedNodes;
-
-                IntroductionNode = new IntroductionNode(IntroductionNodeId, BlindedNodes.First().BlindedPublicKey, BlindingKey, BlindedNodes.First().EncryptedPayload);
-                SubsequentNodes = BlindedNodes.Skip(1);
-                BlindedNodeIds = BlindedNodes.Select(x => x.BlindedPublicKey);
-                EncryptedPayloads = BlindedNodes.Select(x => x.EncryptedPayload);
-            }
-        }
-
-        public record BlindedRouteDetails(BlindedRoute Route, PublicKey LastBlinding);
 
         private readonly ISphinx _sphinx = null;
         private readonly ILightningKeyDerivation _lightningKeyDerivation = null;
@@ -66,50 +69,51 @@ namespace Lyn.Protocol.Bolt4
 
         // note: eclair has separate lists of public keys and encrypted payloads, yet asserts that they are the same length
         // todo: should we do the same? i feel this enumerable of tuples is more elegant but i wonder if there's a 'muh security' reason for the separate lists
-        public BlindedRouteDetails Create(PrivateKey sessionKey, IEnumerable<(PublicKey PublicKey, byte[] Payload)> hops)        
+        public BlindedRouteDetails Create(PrivateKey sessionKey, IEnumerable<(PublicKey PublicKey, byte[] Payload)> hops)
         {
             var e = sessionKey;
-            var blindedHopsAndKeys = hops.Select((hop) => {
+            var blindedHopsAndKeys = hops.Select((hop) =>
+            {
                 var (publicKey, payload) = hop;
                 var blindingKey = _lightningKeyDerivation.PublicKeyFromPrivateKey(e);
-                var sharedSecret = _sphinx.ComputeSharedSecret(publicKey, sessionKey);
+                var sharedSecret = _sphinx.ComputeSharedSecret(publicKey, e);
                 var blindedPublicKey = _sphinx.BlindKey(publicKey, _sphinx.GenerateSphinxKey("blinded_node_id", sharedSecret));
                 var rho = _sphinx.GenerateSphinxKey("rho", sharedSecret);
 
                 // is this right? who knows! i hsould ask chatgpt later
                 // note: i didn't use the ChaCha20Poly1305CipherFunction bc the nonce increments by 1 for each call, and this requires a nonce of 0(?)
                 var cipher = new ChaCha20Poly1305(rho.ToArray());
-                Span<byte> encryptedPayload = stackalloc byte[payload.Length];
-                Span<byte> mac = stackalloc byte[16];
-                cipher.Encrypt(new byte[12], payload.AsSpan(), encryptedPayload, mac, new byte[0]);
+                Span<byte> encryptedPayload = stackalloc byte[payload.Length + 16];
+                Span<byte> ciphertext = stackalloc byte[payload.Length];
+                Span<byte> mac = stackalloc byte[16];    
+                cipher.Encrypt(new byte[12], payload.AsSpan(), ciphertext, mac, new byte[0]);
 
-                // broken: the value for e is not being calculated correctly
-                // broken: this is also likely massively inefficient
-                var keyBytes = blindingKey.GetSpan().ToArray();
-                var sharedSecretArr = sharedSecret.ToArray();
-                var hash = HashGenerator.Sha256(keyBytes.Concat(sharedSecretArr).ToArray());
-                Debug.WriteLine($"hash: {Convert.ToHexString(hash.ToArray())}");
-                var newPrivKey = new PrivateKey(hash.ToArray());
+                ciphertext.CopyTo(encryptedPayload);
+                mac.CopyTo(encryptedPayload.Slice(payload.Length));
+
+                // todo: is blindingKey length a fixed 32 bytes?
+                Span<byte> bytesToHash = stackalloc byte[blindingKey.GetSpan().Length + sharedSecret.Length];
+                blindingKey.GetSpan().CopyTo(bytesToHash);
+                sharedSecret.CopyTo(bytesToHash.Slice(blindingKey.GetSpan().Length));
+                var newPrivKey = new PrivateKey(HashGenerator.Sha256(bytesToHash).ToArray());
                 e = new PrivateKey(_ellipticCureActions.MultiplyWithPrivateKey(newPrivKey, e).ToArray());
-                Debug.WriteLine($"e*PrivKey(hash): {Convert.ToHexString(e)}");
                 return (blindedHop: new BlindedNode(blindedPublicKey, encryptedPayload.ToArray()), blindingKey: blindingKey);
             }).ToList();
 
-            return new BlindedRouteDetails(new BlindedRoute(hops.First().PublicKey, 
-                                                            blindedHopsAndKeys.First().blindingKey, 
-                                                            blindedHopsAndKeys.Select(x => x.blindedHop).ToArray()), 
+            return new BlindedRouteDetails(new BlindedRoute(hops.First().PublicKey,
+                                                            blindedHopsAndKeys.First().blindingKey,
+                                                            blindedHopsAndKeys.Select(x => x.blindedHop).ToArray()),
                                                 blindedHopsAndKeys.Last().blindingKey
                                             );
         }
-        
+
         public PrivateKey DerivePrivateKey(PrivateKey privateKey, PublicKey blindingEphemeralKey)
         {
             // todo: i need some test coverage to verify the multiply logic here
             // note: my guess? way off like in Create ¯\_(ツ)_/¯
             var sharedSecret = _sphinx.ComputeSharedSecret(blindingEphemeralKey, privateKey);
             var generatedKey = new PrivateKey(_sphinx.GenerateSphinxKey("blinded_node_id", sharedSecret).ToArray());
-            var pubKeyForPriv = _lightningKeyDerivation.PublicKeyFromPrivateKey(privateKey);
-            return new PrivateKey(_ellipticCureActions.Multiply(generatedKey, pubKeyForPriv).ToArray());
+            return new PrivateKey(_ellipticCureActions.MultiplyWithPrivateKey(generatedKey, privateKey).ToArray());
         }
 
         public (byte[], PublicKey) DecryptPayload(PrivateKey privateKey, PublicKey blindingEphemeralKey, byte[] encryptedPayload)
